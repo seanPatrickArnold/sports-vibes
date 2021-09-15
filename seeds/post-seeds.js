968bb313328f4e3c684469db7302e2ad2a8f57b0
seanparnold <sean.p.arnold@gmail.com>
2021-09-14 20:15:06 -0700

    copied tech blog

---
+++ seeds/post-seeds.js
@@ -0,0 +1,109 @@
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    body: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    body: 'https://nasa.gov/donec.json',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    body: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    user_id: 1
+  },
+  {
+    title: 'Nunc purus.',
+    body: 'http://desdev.cn/enim/blandit/mi.jpg',
+    user_id: 4
+  },
+  {
+    title: 'Pellentesque eget nunc.',
+    body: 'http://google.ca/nam/nulla/integer.aspx',
+    user_id: 7
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    body: 'https://stanford.edu/consequat.png',
+    user_id: 4
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    body: 'http://edublogs.org/non/ligula/pellentesque.js',
+    user_id: 1
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    body: 'http://ucla.edu/consequat/nulla.html',
+    user_id: 1
+  },
+  {
+    title: 'Duis ac nibh.',
+    body: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    user_id: 9
+  },
+  {
+    title: 'Curabitur at ipsum ac tellus semper interdum.',
+    body: 'https://reverbnation.com/ligula/sit.jpg',
+    user_id: 5
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    body: 'http://china.com.cn/lectus/vestibulum.json',
+    user_id: 3
+  },
+  {
+    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    body: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    user_id: 10
+  },
+  {
+    title: 'Donec dapibus.',
+    body: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    user_id: 8
+  },
+  {
+    title: 'Nulla tellus.',
+    body: 'https://lycos.com/natoque/penatibus/et.html',
+    user_id: 3
+  },
+  {
+    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+    body: 'https://gmpg.org/lorem.jpg',
+    user_id: 3
+  },
+  {
+    title:
+      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
+    body: 'https://paginegialle.it/mattis/egestas.jsp',
+    user_id: 7
+  },
+  {
+    title: 'In hac habitasse platea dictumst.',
+    body: 'http://wikia.com/turpis/eget.jpg',
+    user_id: 6
+  },
+  {
+    title: 'Etiam justo.',
+    body: 'https://shareasale.com/quis.json',
+    user_id: 4
+  },
+  {
+    title: 'Nulla ut erat id mauris vulputate elementum.',
+    body: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    user_id: 6
+  },
+  {
+    title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    body: 'https://java.com/at/nibh/in.png',
+    user_id: 7
+  }
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
